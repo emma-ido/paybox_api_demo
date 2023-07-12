0a9b0d126ca9f0bb4ea60fe7027bba30b6729d52
Emma-Ido Ukpong <emmankelam@gmail.com>
2023-07-12 14:40:22 +0000

    Added functionality for the PayBox widget

--- callback.php
+++ callback.php
@@ -1,36 +1,17 @@
 <?php
 
 $myfile = fopen("dump.txt", "a") or die("Unable to open file!");
-$_POST['hello'] = "hello";
-fwrite($myfile, "\n===== POST ======\n");
+//$_POST['hello'] = "hello";
 
-foreach( $_POST as $stuff ) {
-    if( is_array( $stuff ) ) {
-        foreach( $stuff as $thing ) {
-            echo $thing;
-            fwrite($myfile, $thing);
-        }
-    } else {
-        echo $stuff;
-        fwrite($myfile, $stuff);
-    }
-}
+fwrite($myfile, "\n===== Start ======\n");
 
-fwrite($myfile, "\n====== GET =======\n");
-
-foreach( $_GET as $stuff ) {
-    if( is_array( $stuff ) ) {
-        foreach( $stuff as $thing ) {
-            echo $thing;
-            fwrite($myfile, $thing);
-        }
-    } else {
-        echo $stuff;
-        fwrite($myfile, $stuff);
-    }
-}
+$content = var_dump($_POST);
+// echo $content;
 
 
+fwrite($myfile, $content);
+
+fwrite($myfile, "\n===== End ======\n");
 
 fclose($myfile);
 
